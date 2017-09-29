6868495bded8fdc938b4a503b1739d92941193a8
Kuba <kuba.nowak1993@gmail.com>
2017-09-29 13:08:41 +0200

    Change topic names

--- ras_lab1_motor_controller/src/motor_controller.cpp
+++ ras_lab1_motor_controller/src/motor_controller.cpp
@@ -1,38 +1,45 @@
 #include <ros/ros.h>
-#include <ras_lab1_msgs/PWM.h>
-#include <ras_lab1_msgs/Encoders.h>
+#include <std_msgs/Float32.h>
+#include <phidgets/motor_encoder.h>
 #include <motor_controller.h>
 #include <geometry_msgs/Twist.h>
 
 class MotorController
 {
 private:
-    const double b = 0.23;
-    const double r = 0.0352;
+    const double b = 0.242;
+    const double r = 0.036;
     double e1_sum = 0;
     double e2_sum = 0;
     double kp = 12.0;
     double ki = 5.0;
 public:
     ros::NodeHandle nh;
-    ros::Subscriber encoders_sub, twist_sub;
-    ros::Publisher pwm_pub;
+    ros::Subscriber encoder_left_sub, encoder_right_sub, twist_sub;
+    ros::Publisher vel_left_pub, vel_right_pub;
 
     geometry_msgs::Twist twist;
-    ras_lab1_msgs::Encoders encoders;
+    phidgets::motor_encoder encoder_left, encoder_right;
 
     MotorController()
     {
-        encoders_sub = nh.subscribe("/kobuki/encoders", 1, &MotorController::EncodersCallback, this);
+        encoder_left_sub = nh.subscribe("/motorcontrol/encoder_left", 1, &MotorController::EncoderLeftCallback, this);
+        encoder_right_sub = nh.subscribe("/motorcontrol/encoder_right", 1, &MotorController::EncoderRightCallback, this);
 
         twist_sub = nh.subscribe("/motor_controller/twist", 1, &MotorController::TwistCallback, this);
 
-        pwm_pub = nh.advertise<ras_lab1_msgs::PWM>("/kobuki/pwm", 1);
+        vel_right_pub = nh.advertise<std_msgs::Float32>("/motorcontrol/cmd_vel_right", 1);
+        vel_left_pub = nh.advertise<std_msgs::Float32>("/motorcontrol/cmd_vel_left", 1);
     }
 
-    void EncodersCallback(const ras_lab1_msgs::Encoders::ConstPtr &msg)
+    void EncoderLeftCallback(const phidgets::motor_encoder::ConstPtr &msg)
     {
-        encoders = *msg;
+        encoder_left = *msg;
+    }
+
+    void EncoderRightCallback(const phidgets::motor_encoder::ConstPtr &msg)
+    {
+        encoder_right = *msg;
     }
 
     void TwistCallback(const geometry_msgs::Twist::ConstPtr &msg)
@@ -40,7 +47,7 @@ public:
         twist = *msg;
     }
 
-    void UpdatePWM()
+    void UpdateMotorControl()
     {
         double v = twist.linear.x;
         double w = twist.angular.z;
@@ -48,8 +55,8 @@ public:
         double w1_ref = 0.5 * (2 * v + w * b) / r;
         double w2_ref = 0.5 * (2 * v - w * b) / r;
 
-        double w1 = ((double)(encoders.delta_encoder1)) * 3.1415 / 180.0 * 10.0;
-        double w2 = ((double)(encoders.delta_encoder2)) * 3.1415 / 180.0 * 10.0;
+        double w1 = ((double)(encoder_right.count_change)) * 3.1415 / 180.0 * 10.0;
+        double w2 = ((double)(encoder_left.count_change)) * 3.1415 / 180.0 * 10.0;
 
         double e1 = w1_ref - w1;
         double e2 = w2_ref - w2;
@@ -57,14 +64,14 @@ public:
         e1_sum += e1 * 0.1;
         e2_sum += e2 * 0.1;
 
-        double u1 = kp * e1 + (ki + 1.0) * e1_sum;//left wheel must get higher voltage to match right one's response speed
+        double u1 = kp * e1 + ki * e1_sum;
         double u2 = kp * e2 + ki * e2_sum;
 
-        ras_lab1_msgs::PWM pwm;
-        pwm.PWM1 = (int)(u1);
-        pwm.PWM2 = (int)(u2);
-
-        pwm_pub.publish(pwm);
+	std_msgs::Float32 f1, f2;
+	f1.data = (float)u1;
+	f2.data = (float)u2;
+        vel_right_pub.publish(f1);
+        vel_left_pub.publish(f2);
     }
 };
 
@@ -76,7 +83,7 @@ int main(int argc, char** argv)
     ros::Rate loop_rate(10);
     while(mc.nh.ok())
     {
-        mc.UpdatePWM();
+        mc.UpdateMotorControl();
         ros::spinOnce();
         loop_rate.sleep();
     }
