af0062369dbe46c70a21bbe33a09a13a2dd89243
Jiajun <montemiracle@live.com>
2017-10-04 20:35:15 +0200

    Controller modifications - it stops well

--- ras_lab1_motor_controller/src/motor_controller.cpp
+++ ras_lab1_motor_controller/src/motor_controller.cpp
@@ -9,10 +9,11 @@ double kp_left;
 double ki_left;
 double kp_right;
 double ki_right;
+double dead_region = 8.0;
 int frequency;
 double prev_time = -1;
-double prev_left = 10;
-double prev_right = 10;
+double prev_left = 0;
+double prev_right = 0;
 int accumulated_left;
 int accumulated_right;
 
@@ -94,25 +95,35 @@ public:
         double w1 = -((double)(accumulated_right)) * 2 * 3.14159 / (encoder_res * delta_time);
         double w2 = ((double)(accumulated_left)) * 2 * 3.14159 / (encoder_res * delta_time);
 
-		ROS_INFO("delta time: %f, er: %d, el: %d", delta_time,encoder_right.count_change, encoder_left.count_change);
+		//ROS_INFO("delta time: %f, er: %d, el: %d", delta_time,encoder_right.count_change, encoder_left.count_change);
 
         double e1 = w1_ref - w1;
         double e2 = w2_ref - w2;
         ROS_INFO("w1 ref: %f, w1: %f", w1_ref, w1);
+        ROS_INFO("w2 ref: %f, w2: %f", w2_ref, w2);
         //ROS_INFO("w2 ref: %f, w2: %f", w2_ref, w2);
 
         e1_sum += e1 * delta_time;
         e2_sum += e2 * delta_time;
 
-       	prev_right += kp_right * e1 + ki_right * e1_sum;
-        prev_left += kp_left * e2 + ki_left * e2_sum;
+		double acc1, acc2;
+		acc1 = kp_right * e1 + ki_right * e1_sum;
+		acc2 = kp_left * e2 + ki_left * e2_sum;
+
+       	prev_right += acc1;
+        prev_left += acc2;
+        
+        if ( w1_ref < 0.01 && w1_ref > -0.01 && prev_right < dead_region && prev_right > -dead_region)
+        { prev_right = 0.0;}
+        if ( w2_ref < 0.01 && w2_ref > -0.01 && prev_left < dead_region && prev_left > -dead_region)
+        { prev_left = 0.0;}
 
 		std_msgs::Float32 f1, f2;
 		f1.data = -(float)prev_right;
 		f2.data = (float)prev_left;
         vel_right_pub.publish(f1);
         vel_left_pub.publish(f2);
-        ROS_INFO("f1 ref: %f, f2: %f", f1.data, f2.data);
+        ROS_INFO("f1: %f, f2: %f", f1.data, f2.data);
         
         accumulated_left = 0;
     	accumulated_right = 0;
