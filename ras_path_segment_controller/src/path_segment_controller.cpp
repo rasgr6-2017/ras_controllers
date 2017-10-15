b8d3a8e60844fc11147e3986ddd11e14a3d42b11
Kuba <kuba.nowak1993@gmail.com>
2017-10-16 01:17:53 +0200

    Path segment controller improvement

--- ras_path_segment_controller/src/path_segment_controller.cpp
+++ ras_path_segment_controller/src/path_segment_controller.cpp
@@ -10,21 +10,22 @@ geometry_msgs::PoseStamped goal_pose, start_pose, current_pose;
 ros::Subscriber pose_sub;
 ros::Publisher twist_pub;
 
-double theta_goal, theta_start, phi;
-double v_max, w_max, kp;
+double theta_goal, theta_start, phi, theta_before_end_turn;
+double v_max, w_max, kp, x_vmax = 0.05, x_wmax = 0.05;
 double e;
+double r;
 
 bool turn_start_complete = true, translation_complete = true, turn_end_complete = true;
 
 typedef actionlib::SimpleActionServer<ras_path_segment_controller::GoToPoseAction> Server;
 
-double GetDirection(geometry_msgs::Pose start, geometry_msgs::Pose goal){
+void GetDirectionAndDistance(geometry_msgs::Pose start, geometry_msgs::Pose goal){
     double x1 = start.position.x;
     double y1 = start.position.y;
     double x2 = goal.position.x;
     double y2 = goal.position.y;
-    double phi = atan2(y2-y1, x2-x1);
-    return phi;
+    phi = atan2(y2-y1, x2-x1);
+    r = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
 }
 
 void GoalCallback(Server *s){
@@ -32,7 +33,7 @@ void GoalCallback(Server *s){
     theta_goal = 2 * asin(goal_pose.pose.orientation.z);
     start_pose = current_pose;
     theta_start = 2 * asin(start_pose.pose.orientation.z);
-    phi = GetDirection(start_pose.pose, goal_pose.pose);
+    GetDirectionAndDistance(start_pose.pose, goal_pose.pose);
 
     turn_start_complete = translation_complete = turn_end_complete = false;
 }
@@ -60,33 +61,49 @@ bool Equal(double a, double b){
     return fabs(a - b)<e;
 }
 
+double GetVel(double x_actual, double x_final, double x_start, double ramp_length, double vel_max){
+    double vel;
+    double C = vel_max / ramp_length;
+    vel = fabs(x_actual-x_start) < 0.5 * fabs(x_final-x_start) ? vel_max : C * fabs(x_final - x_actual);
+    vel = vel > vel_max ? vel_max : vel;
+    vel = x_final - x_actual > 0 ? vel : -vel;
+    return vel;
+}
+
 void UpdateRobotTwist(){
     double v = 0.0, w = 0.0;
     double x = current_pose.pose.position.x;
     double y = current_pose.pose.position.y;
     double theta = 2 * asin(current_pose.pose.orientation.z);
 
+    double d = sqrt((x - start_pose.pose.position.x)*(x - start_pose.pose.position.x)
+                    + (y - start_pose.pose.position.y)*(y - start_pose.pose.position.y));
+
     if(!turn_end_complete){
         if(!translation_complete){
             if(!turn_start_complete){
-                if(!Equal(theta, phi))
-                    w = 0.5 * (phi - theta_start > 0) ? w_max : -w_max;
+                if(!Equal(theta, phi)){
+                    w = GetVel(theta, phi, theta_start, x_wmax, w_max);
+                }
                 else{
                     turn_start_complete = true;
                 }
             }
             else{
-                if(!Equal(x, goal_pose.pose.position.x) || !Equal(y, goal_pose.pose.position.y))
-                    v = 0.5 * v_max;
+                if(!Equal(d, r)){
+                    v = GetVel(d, r, 0.0, x_vmax, 0.5*v_max);
+                }
                 else{
+                    theta_before_end_turn = theta;
                     translation_complete = true;
                 }
             }
         }
         else{
             if(turn_start_complete){
-                if(!Equal(theta, theta_goal))
-                    w = 0.5 * (theta_goal - phi > 0) ? w_max : -w_max;
+                if(!Equal(theta, theta_goal)){
+                    w = GetVel(theta, theta_goal, theta_before_end_turn, x_wmax, w_max);
+                }
                 else{
                     turn_end_complete = true;
                 }
